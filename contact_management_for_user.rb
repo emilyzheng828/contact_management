2ba480e4383b7df73300b797d25ae403b5c35cd6
Emily_rrz <emily.zheng828@gmail.com>
2015-12-02 15:44:08 +0000

    update bug fixed

--- contact_management_for_user.rb
+++ contact_management_for_user.rb
@@ -28,37 +28,41 @@ class InterfaceUser
   def edit_a_contact
     print @database.list
     puts "give me the number of contact you want to update"
-    to_be_edited_index = gets.chomp
-    puts @database[to_be_edited_index.to_i]
+    to_be_edited_index_input = gets.chomp
+    to_be_edited_index = to_be_edited_index_input.to_i
     puts "what do you want to change? \n
     AA forename \n
     BB Surname \n
     CC Telephone \n
-    DD Address"
+    DD Address".green
     user_choice_of_item = gets.chomp
-    update_contact_item_index =
-    0 if user_choice_of_item == "AA"
-    1 if user_choice_of_item == "BB"
-    2 if user_choice_of_item == "CC"
-    3 if user_choice_of_item == "DD"
+     
+    update_contact_item_index = 0 if user_choice_of_item == "AA"
+    update_contact_item_index = 1 if user_choice_of_item == "BB"
+    update_contact_item_index = 2 if user_choice_of_item == "CC"
+    update_contact_item_index = 3 if user_choice_of_item == "DD"
+    
     puts "what do you want to change it to ?"
-    update_information = gets.chomp.to_s
-    @database[to_be_edited_index.to_i][update_contact_item_index] = update_information 
+    update_information = gets.chomp
+    @database[to_be_edited_index][update_contact_item_index] = update_information.to_s
   end
 
   def delete_a_contact
     print @database.list
+    if @database.count > 0
     puts "give me the number of contact you want to delete"
     to_be_deleted_index = gets.chomp.to_i
     @database.delete(to_be_deleted_index)
     return @database.list
+    else puts "there in nothing in the list right now".red
+    end
   end
 
   def run
-    puts "A. List my contact".blue
-    puts "B. Create new contact".blue
-    puts "C. Edit a contact".blue
-    puts "D. Delete a contact".blue
+    puts "A. List my contact".green
+    puts "B. Create new contact".green
+    puts "C. Edit a contact".green
+    puts "D. Delete a contact".green
     while true 
       user_choice = gets.chomp
       if user_choice == "A"
@@ -76,7 +80,6 @@ class InterfaceUser
       break if user_choice ==""
     end
   end
-
 end 
 
 user_action = InterfaceUser.new 
